d0012c5a0beebf9a5b51cd19e6404f5d8ab72726
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2018-06-09 16:02:43 +0200

    use moment duration to decrement time

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,21 +1,36 @@
 <template>
   <div>
     <p class="timer">
-      {{ timer }}
+      {{ displayedTimer }}
     </p>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   data () {
     return {
-      timer: null
+      timer: moment.duration(1, 'minutes')
+    }
+  },
+
+  computed: {
+    displayedTimer () {
+      // return this.timer.as('minutes')
+      return this.timer.as('seconds')
     }
   },
 
   mounted () {
-    this.timer = '00:00'
+    const interval = setInterval(() => {
+      this.timer.subtract(1, 'second')
+
+      if (this.timer.get('seconds') === 0) {
+        clearInterval(interval)
+      }
+    }, 1000)
   }
 }
 </script>
