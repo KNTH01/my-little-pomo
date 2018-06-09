55bd18af4127ba0acfb81a444df40944e2f6387f
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2018-06-09 15:26:39 +0200

    load bulma and bueify

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import Buefy from 'buefy'
+
+Vue.use(Buefy)
+
 Vue.config.productionTip = false
 
 new Vue({
